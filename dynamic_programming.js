90308132ded76d4fa1a8b1a8fba9aeeadd005062
luisVargasGu <vargasluis012@gmail.com>
2022-12-26 22:38:46 -0500

    Implemented the bestSum function.

--- dynamic_programming.js
+++ dynamic_programming.js
@@ -91,11 +91,11 @@ function howSum(targetSum, numbers) {
 	return null
 }
 
-console.log('howSum:', howSum(7,[5,3,4,7]))
-console.log('howSum:', howSum(7,[2,7]))
-console.log('howSum:', howSum(100,[5,3,4,7]))
-console.log('howSum:', howSum(1,[5,3,4,7]))
-console.log('howSum:', howSum(0,[5,3,4,7]))
+// console.log('howSum:', howSum(7,[5,3,4,7]))
+// console.log('howSum:', howSum(7,[2,7]))
+// console.log('howSum:', howSum(100,[5,3,4,7]))
+// console.log('howSum:', howSum(1,[5,3,4,7]))
+// console.log('howSum:', howSum(0,[5,3,4,7]))
 // console.log('howSum:', howSum(300,[14,7]))
 
 
@@ -115,16 +115,88 @@ function howSumMemo(targetSum, numbers, memo = {}) {
 	return null
 }
 
-console.log('howSumMemo:', howSumMemo(7,[5,3,4,7]))
-console.log('howSumMemo:', howSumMemo(7,[2,7]))
-console.log('howSumMemo:', howSumMemo(100,[5,3,4,7]))
-console.log('howSumMemo:', howSumMemo(1,[5,3,4,7]))
-console.log('howSumMemo:', howSumMemo(0,[5,3,4,7]))
-console.log('howSumMemo:', howSumMemo(300,[14,7]))
+// console.log('howSumMemo:', howSumMemo(7,[5,3,4,7]))
+// console.log('howSumMemo:', howSumMemo(7,[2,7]))
+// console.log('howSumMemo:', howSumMemo(100,[5,3,4,7]))
+// console.log('howSumMemo:', howSumMemo(1,[5,3,4,7]))
+// console.log('howSumMemo:', howSumMemo(0,[5,3,4,7]))
+// console.log('howSumMemo:', howSumMemo(300,[14,7]))
 
+//my working solution
+function bestSum(targetSum, numbers) {
+	if (targetSum == 0) { return [] }
+	if (targetSum < 0) { return null }
+	const finalOut = [];
+	for (let num of numbers) {
+		let posValue = howSum(targetSum-num, numbers);
+		if (posValue != null) {
+			finalOut.push([...posValue, num]);
+		}
+	}
+	let bestArray = finalOut[1];
+	for (var i = finalOut.length - 1; i >= 1; i--) {
+		if (finalOut[i].length < bestArray.length) {
+			bestArray = finalOut[i];
+		}
+	}
+	return bestArray;
+}
 
+// console.log('bestSum', bestSum(8, [2,3,5]))
+// console.log('bestSum', bestSum(7, [5,3,7,4]))
+// console.log('bestSum', bestSum(1, [5,3,7,4]))
+// console.log('bestSum', bestSum(0, [5,3,7,4]))
+// console.log('bestSum', bestSum(300, [14, 7]))
 
+//my working memoized solution (first)
+function bestSumMemo(targetSum, numbers, memo = {}) {
+	if (memo[targetSum]) { return memo[targetSum] }
+	if (targetSum == 0) { return [] }
+	if (targetSum < 0) { return null }
+	const finalOut = [];
+	for (let num of numbers) {
+		let posValue = howSum(targetSum-num, numbers, memo);
+		memo[targetSum] = posValue
+		if (posValue != null) {
+			finalOut.push([...posValue, num]);
+		}
+	}
+	let bestArray = finalOut[1];
+	for (var i = finalOut.length - 1; i >= 1; i--) {
+		if (finalOut[i].length < bestArray.length) {
+			bestArray = finalOut[i];
+		}
+	}
+	return bestArray;
+}
 
+// Comments on my work:
+// 1. It's supposed to return null if it's not possible and currently it returns undefined
+
+//my working memoized solution (final)
+function bestSumMemo(targetSum, numbers, memo = {}) {
+	if (memo[targetSum]) { return memo[targetSum] }
+	if (targetSum == 0) { return [] }
+	if (targetSum < 0) { return null }
+	let bestArray = null;
+	for (let num of numbers) {
+		let posValue = howSum(targetSum-num, numbers, memo);
+		memo[targetSum] = posValue
+		if (posValue != null) {
+			const combination = [...posValue, num];
+			if (bestArray == null || combination.length < bestArray.length) {
+				bestArray = combination;
+			}
+		}
+	}
+	return bestArray;
+}
+
+console.log('bestSumMemo', bestSumMemo(8, [2,3,5]))
+console.log('bestSumMemo', bestSumMemo(7, [5,3,7,4]))
+console.log('bestSumMemo', bestSumMemo(1, [5,3,7,4]))
+console.log('bestSumMemo', bestSumMemo(300, [14, 7]))
+console.log('bestSumMemo', bestSumMemo(3000, [1,3,7,4]))
 
 
 
